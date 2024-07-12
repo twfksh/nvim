35beb15adfbf685fb13222634fb60df73b97b721
Toufiq A. Shishir <toufiq_a24@outlook.com>
2024-07-12 16:25:50 +0600

    -

--- lua/lspconfig/auto_format.lua
+++ lua/lspconfig/auto_format.lua
@@ -26,7 +26,7 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
+      python = { 'ruff' },
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
