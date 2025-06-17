9a88692ee64a42d00f238dab87f151cb73046d10
Toufiq Shishir <toufiq_a24@outlook.com>
2025-06-17 16:02:02 +0600

    update: added and removed a few colorschemes

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -1,12 +1,37 @@
 return {
   'tjdevries/colorbuddy.nvim',
   dependencies = {
-    { 'echasnovski/mini.base16', version = '*' },
+    { 'echasnovski/mini.base16',  version = '*' },
     { 'EdenEast/nightfox.nvim' },
     { 'sainnhe/everforest' },
     { 'rebelot/kanagawa.nvim' },
+    { "rose-pine/neovim",         name = "rose-pine" },
+    { "ellisonleao/gruvbox.nvim", priority = 1000,   config = true, opts = ... },
+    { "savq/melange-nvim" },
+    {
+      'everviolet/nvim',
+      name = 'evergarden',
+      priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
+      opts = {
+        theme = {
+          variant = 'fall', -- 'winter'|'fall'|'spring'|'summer'
+          accent = 'green',
+        },
+        editor = {
+          transparent_background = false,
+          sign = { color = 'none' },
+          float = {
+            color = 'mantle',
+            invert_border = false,
+          },
+          completion = {
+            color = 'surface0',
+          },
+        },
+      }
+    }
   },
   config = function()
-    vim.cmd [[colorscheme terafox]]
+    vim.cmd [[colorscheme melange]]
   end,
 }
