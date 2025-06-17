734133d00c89274592e8756854970faa72971b15
Toufiq Shishir <toufiq_a24@outlook.com>
2025-06-17 20:33:29 +0600

    update: cleaned up the config & added lua_ls config

--- lua/lspconfig/lspconfig.lua
+++ lua/lspconfig/lspconfig.lua
@@ -14,58 +14,13 @@ local LSPConf = {
   -- Auto-Complete
   {
     'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
     dependencies = {
       { 'rafamadriz/friendly-snippets' },
     },
-
-    -- use a release tag to download pre-built binaries
     version = '1.*',
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
     opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-n/C-p or Up/Down: Select next/previous item
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help (if signature.enabled = true)
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'default' },
-
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
-      },
-
-      -- (Default) Only show the documentation popup when manually triggered
-      completion = { documentation = { auto_show = false } },
-
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
-      },
-
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
-      fuzzy = { implementation = "prefer_rust_with_warning" }
+      keymap = { preset = 'super-tab' },
     },
-    opts_extend = { "sources.default" }
   },
 
   -- LSP
@@ -80,6 +35,13 @@ local LSPConf = {
     },
     config = function()
       local lsp_zero = require 'lsp-zero'
+
+      local original_capabilities = vim.lsp.protocol.make_client_capabilities()
+      local capabilities = vim.tbl_deep_extend(
+        'force',
+        original_capabilities,
+        require('blink.cmp').get_lsp_capabilities({}, false)
+      )
 
       -- lsp_attach is where you enable features that only work
       -- if there is a language server active in the file
@@ -100,11 +62,11 @@ local LSPConf = {
       lsp_zero.extend_lspconfig {
         sign_text = true,
         lsp_attach = lsp_attach,
-        capabilities = require('blink.cmp').get_lsp_capabilities(),
+        capabilities = capabilities,
       }
 
       require('mason-lspconfig').setup {
-        ensure_installed = {},
+        ensure_installed = { "lua_ls" },
         handlers = {
           -- this first function is the "default handler"
           -- it applies to every language server without a "custom handler"
@@ -113,6 +75,43 @@ local LSPConf = {
           end,
         },
       }
+
+      vim.lsp.config('lua_ls', {
+        on_init = function(client)
+          if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if
+                path ~= vim.fn.stdpath('config')
+                and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+            then
+              return
+            end
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              version = 'LuaJIT',
+              path = {
+                'lua/?.lua',
+                'lua/?/init.lua',
+              },
+            },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME
+                -- Depending on the usage, you might want to add additional paths
+                -- here.
+                -- '${3rd}/luv/library'
+                -- '${3rd}/busted/library'
+              }
+            }
+          })
+        end,
+        settings = {
+          Lua = {}
+        }
+      })
 
       vim.keymap.set('n', '<leader>i', '<cmd>LspInfo<cr>', opts)
       vim.keymap.set('n', '<leader>I', '<cmd>LspInstall<cr>', opts)
