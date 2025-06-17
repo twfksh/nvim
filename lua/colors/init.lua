6b56b43c05cdf9c06f9d6fd8b27aa22f6bc77866
Toufiq Shishir <toufiq_a24@outlook.com>
2025-06-17 22:53:15 +0600

    update: removed default options

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -6,30 +6,9 @@ return {
     { 'sainnhe/everforest' },
     { 'rebelot/kanagawa.nvim' },
     { "rose-pine/neovim",         name = "rose-pine" },
-    { "ellisonleao/gruvbox.nvim", priority = 1000,   config = true, opts = ... },
+    { "ellisonleao/gruvbox.nvim", priority = 1000,     config = true,  opts = ... },
     { "savq/melange-nvim" },
-    {
-      'everviolet/nvim',
-      name = 'evergarden',
-      priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
-      opts = {
-        theme = {
-          variant = 'fall', -- 'winter'|'fall'|'spring'|'summer'
-          accent = 'green',
-        },
-        editor = {
-          transparent_background = false,
-          sign = { color = 'none' },
-          float = {
-            color = 'mantle',
-            invert_border = false,
-          },
-          completion = {
-            color = 'surface0',
-          },
-        },
-      }
-    },
+    { 'everviolet/nvim',          name = 'evergarden', priority = 1000 },
   },
   config = function()
     vim.cmd [[colorscheme terafox]]
